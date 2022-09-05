1ca284fad160b6ef1bb1117c46446c356f8565df
Sheetal Kanago <91463047+sheetal-Kanago@users.noreply.github.com>
2022-09-05 19:40:09 +0530

    Create Bank.sol

---
+++ Sol/Bank.sol
@@ -0,0 +1,6 @@
+     1. Add new bank to Blockchain ledger:
+
+This function is used by the admin to add a new bank to the KYC Contract. This function can be called by admin only. This function takes the below input parameters:
+
+bankName of string type: The name of the bank
+address of address type: The unique Ethereum address of the bank
